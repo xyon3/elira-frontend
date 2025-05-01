71b5a1b8d47fe97b9ce68f00a5e9604d811680a2
xyon3 <reyespauldavid@gmail.com>
2025-05-02 07:00:56 +0800

    fix: required suspense on urlSearchParanms

--- src/app/lib/components/header.tsx
+++ src/app/lib/components/header.tsx
@@ -2,13 +2,17 @@
 import { PanelLeft, Upload } from "lucide-react";
 import { LoginModal } from "./login-modal";
 import { useRouter } from "next/navigation";
+import { Suspense } from "react";
 
 export function Header() {
     const router = useRouter();
 
     return (
         <header className="flex justify-end w-full bg-red ml-auto items-center gap-6">
-            <LoginModal />
+            <Suspense>
+                <LoginModal />
+            </Suspense>
+
             <div className="text-2xl font-bold mr-auto flex items-center gap-2">
                 <PanelLeft className="hidden" color="#ffffff" />
                 <h1 className="">ELIRA</h1>
