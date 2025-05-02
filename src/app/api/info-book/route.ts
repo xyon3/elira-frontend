a845371d696608f225d81850b4912d5922bc4486
xyon3 <reyespauldavid@gmail.com>
2025-05-03 07:26:25 +0800

    api interfacec for uploading

---
+++ src/app/api/info-book/route.ts
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { NextResponse, type NextRequest } from "next/server";
+
+axios.defaults.baseURL = process.env.ELIRA_BACKEND;
+
+export async function POST(req: NextRequest) {
+    const body = await req.json();
+
+    const data = {
+        ...body,
+        uploader: "erick.goneda@ushg.phs",
+    };
+
+    const response = await axios({
+        method: "POST",
+        url: "/api/books",
+        data,
+    });
+
+    return NextResponse.json({ data, referenceID: response.data.id });
+}
