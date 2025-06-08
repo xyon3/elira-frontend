f6d6a2238a984a8bb35d3dfbbda815840b02bd61
xyon3 <reyespauldavid@gmail.com>
2025-06-08 12:24:26 +0800

    update

--- src/app/manage/page.tsx
+++ src/app/manage/page.tsx
@@ -1,7 +1,19 @@
 import { Columns3Cog, FileCog, FolderCog, School } from "lucide-react";
+import { cookies } from "next/headers";
 import Link from "next/link";
 
-export default function Repository() {
+export default async function Repository() {
+    const cookieStore = await cookies();
+    const token = cookieStore.get("token");
+
+    if (!token) {
+        return <>Not Allowed</>;
+    }
+
+    const identity = JSON.parse(token.value);
+
+    console.log(identity);
+
     const buttonStyle =
         "btn btn-ghost flex flex-col text-3xl h-full btn-neutral border border-black/10 py-16 px-16  bg-base-100/75 hover:bg-black/75";
     const iconSize = 98;
@@ -9,19 +21,27 @@ export default function Repository() {
     return (
         <div className="flex justify-start">
             <div className="grid grid-cols-4 gap-6">
-                <Link href="/manage/library" className={buttonStyle}>
-                    <Columns3Cog size={iconSize} />
-                    Library
-                </Link>
-
                 <Link href="/manage/repository" className={buttonStyle}>
                     <FolderCog size={iconSize} />
                     Repository
                 </Link>
-                <Link href="/manage/departments" className={buttonStyle}>
-                    <School size={iconSize} />
-                    Departments
-                </Link>
+
+                {(identity.role == "custodian" || identity.role) ==
+                "uploader" ? (
+                    <>
+                        <Link href="/manage/library" className={buttonStyle}>
+                            <Columns3Cog size={iconSize} />
+                            Library
+                        </Link>
+                        <Link
+                            href="/manage/departments"
+                            className={buttonStyle}
+                        >
+                            <School size={iconSize} />
+                            Departments
+                        </Link>
+                    </>
+                ) : null}
             </div>
         </div>
     );
